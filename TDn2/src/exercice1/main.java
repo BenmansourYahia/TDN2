6fea817024deaf4b435e7c509561013161f2324d
YAHIA benmansour <yahiabenmansour764@gmail.com>
2023-12-18 21:21:16 +0100

    first commit

---
+++ TDn2/src/exercice1/main.java
@@ -0,0 +1,12 @@
+package exercice1;
+public class main {
+public static void main(String[] args) {
+		Point pointA=new Point('A',4.0);
+		    System.out.println("le point avant translation:");
+		    pointA.affiche();
+		    pointA.translate(4);
+		    System.out.println("le point après translation:");
+		    pointA.affiche();
+		        }}
+
+
